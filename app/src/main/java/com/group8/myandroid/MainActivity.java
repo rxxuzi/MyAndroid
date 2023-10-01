6f55e68270c460570de3049463a0ffeb990b3454
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-10-01 14:49:47 +0900

    Bug fixed

--- app/src/main/java/com/group8/myandroid/MainActivity.java
+++ app/src/main/java/com/group8/myandroid/MainActivity.java
@@ -1,7 +1,10 @@
 package com.group8.myandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // マップ表示ボタンのインスタンスを取得
+        Button mapButton = findViewById(R.id.mapButton);
+
+        // ボタンのクリックリスナーを設定
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // MapActivityへのインテントを作成
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+
+                // MapActivityを開始
+                startActivity(intent);
+            }
+        });
     }
 }
