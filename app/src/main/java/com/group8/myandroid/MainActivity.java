bfe59b91d087246cd69a9d1e580dc62f168d1a86
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-10-07 19:11:15 +0900

    Migrated to new UI and added ability to browse from database and add to list

--- app/src/main/java/com/group8/myandroid/MainActivity.java
+++ app/src/main/java/com/group8/myandroid/MainActivity.java
@@ -2,6 +2,10 @@ package com.group8.myandroid;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.group8.myandroid.database.DatabaseHelper;
+import com.group8.myandroid.database.DatabaseManager;
 import com.group8.myandroid.database.Shop;
 import com.group8.myandroid.database.Shops;
 import com.group8.myandroid.global.EasyLogger;
@@ -17,20 +21,27 @@ import com.group8.myandroid.global.EasyLogger;
  */
 public class MainActivity extends AppCompatActivity {
 
-    EasyLogger logger = new EasyLogger("XYZ" , true);
+    EasyLogger logger = new EasyLogger(this, true);
+    public static final boolean LOAD = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DatabaseManager.dbCleanUp(this);
+
         // JsonからDBにデータをパース
         DatabaseManager dbManager = new DatabaseManager(this);
 
-        try {
-            dbManager.loadShopsFromJson();
-            dbManager.addShopsArray();
-        } catch (Exception e) {
-            logger.error(e);
+        if(LOAD){
+            try {
+                // Shopsデータのロード
+                dbManager.loadShopsFromJson();
+                dbManager.addShopsArray();
+            } catch (Exception e) {
+                logger.error(e);
+            }
         }
 
         DatabaseHelper dbHelper = new DatabaseHelper(this);
@@ -38,6 +49,21 @@ public class MainActivity extends AppCompatActivity {
         for (Shop shop : Shops.shops) {
             logger.debug(shop.toString());
         }
+
+        logger.debug("Shops.shops.size() = " + Shops.shops.size());
+
+        LocationProvider lp = new LocationProvider(this);
+
+        // RecyclerViewのインスタンスを取得
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        // LayoutManagerをセット（ここではLinearLayoutManagerを使用）
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // ShopAdapterを作成し、RecyclerViewにセット
+        ShopAdapter shopAdapter = new ShopAdapter(Shops.shops);
+        recyclerView.setAdapter(shopAdapter);
+
 
     }
 }
