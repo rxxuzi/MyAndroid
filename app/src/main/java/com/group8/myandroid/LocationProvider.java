33a754b3d2d7a77c303cee6486eba93013918948
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-10-09 00:28:30 +0900

    Bug fixed

--- app/src/main/java/com/group8/myandroid/LocationProvider.java
+++ app/src/main/java/com/group8/myandroid/LocationProvider.java
@@ -10,30 +10,37 @@ import com.group8.myandroid.global.EasyLogger;
 
 /**
  * <h1>LocationProvider</h1>
- * This class provides location updates.
- * @since 1.0.4
+ * Provides functionality to get the device's current location.
+ * @see LocationListener
+ * @see LocationManager
  * @author rxxuzi
  */
 public class LocationProvider implements LocationListener {
 
     private LocationManager locationManager;
-    private static final long MIN_TIME = 1000;  // 更新時間（ミリ秒）
-    private static final float MIN_DISTANCE = 1; // 更新距離（メートル）
+    private Location currentLocation;
+    private static final long UPDATE_TIME = 1000;  // 更新時間（ミリ秒）
+    private static final float UPDATE_DISTANCE = 1; // 更新距離（メートル）
 
     public LocationProvider(Context context) {
         locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
     }
 
+
+    /**
+     * Starts location updates. Should be called when the application starts or when it is
+     * brought to the foreground.
+     */
     public void startLocationUpdates() {
         try {
             locationManager.requestLocationUpdates(
-                    LocationManager.GPS_PROVIDER,
-                    MIN_TIME,
-                    MIN_DISTANCE,
-                    this
+                    LocationManager.GPS_PROVIDER, // Use GPS for location updates.
+                    UPDATE_TIME,        // Update interval in milliseconds.
+                    UPDATE_DISTANCE,    // Minimum distance change for updates, in meters.
+                    locationListener    // Listener for receiving updates.
             );
         } catch (SecurityException e) {
-            new EasyLogger().error(e);
+            Log.e("LocationProvider", "Location permissions not granted.", e);
         }
     }
 
@@ -43,23 +50,41 @@ public class LocationProvider implements LocationListener {
 
     @Override
     public void onLocationChanged(Location location) {
-        Log.d("Location", "Latitude: " + location.getLatitude());
-        Log.d("Location", "Longitude: " + location.getLongitude());
+        new EasyLogger(this).debug("onLocationChanged: " + location.getLatitude() + ", " + location.getLongitude());
     }
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        // 位置プロバイダの状態が変わった時の処理
+    /**
+     * Retrieves the last known location of the device.
+     *
+     * @return the last known location, or null if no location is available.
+     */
+    public Location getCurrentLocation() {
+        return currentLocation;
     }
 
-    @Override
-    public void onProviderEnabled(String provider) {
-        // 位置プロバイダが有効になった時の処理
-    }
+    // Create a LocationListener to receive location updates.
+    private final LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            // Update the currentLocation variable whenever a new location is received.
+            currentLocation = location;
+            Log.d(this.getClass().getName(), "onLocationChanged: " + location.getLatitude() + ", " + location.getLongitude());
+        }
 
-    @Override
-    public void onProviderDisabled(String provider) {
-        // 位置プロバイダが無効になった時の処理
-    }
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // Implement as needed.
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // Implement as needed.
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            // Implement as needed.
+        }
+    };
 }
 
--- app/src/main/java/com/group8/myandroid/MainActivity.java
+++ app/src/main/java/com/group8/myandroid/MainActivity.java
@@ -1,7 +1,12 @@
 package com.group8.myandroid;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.group8.myandroid.database.DatabaseHelper;
@@ -9,6 +14,9 @@ import com.group8.myandroid.database.DatabaseManager;
 import com.group8.myandroid.database.Shop;
 import com.group8.myandroid.database.Shops;
 import com.group8.myandroid.global.EasyLogger;
+import android.Manifest;
+import android.content.pm.PackageManager;
+
 
 /**
  * <h1>MainActivity</h1>
@@ -22,6 +30,8 @@ import com.group8.myandroid.global.EasyLogger;
 public class MainActivity extends AppCompatActivity {
 
     EasyLogger logger = new EasyLogger(this, true);
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    private LocationProvider locationProvider;
     public static final boolean LOAD = true;
 
     @Override
@@ -46,13 +56,34 @@ public class MainActivity extends AppCompatActivity {
 
         DatabaseHelper dbHelper = new DatabaseHelper(this);
 
-        for (Shop shop : Shops.shops) {
-            logger.debug(shop.toString());
+        debugLog();
+
+        // LocationProvider の初期化
+        locationProvider = new LocationProvider(this);
+
+        // パーミッションがある場合のみ、位置情報を取得
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // 位置情報をログに出力
+            Location currentLocation = locationProvider.getCurrentLocation();
+            if (currentLocation != null) {
+                logger.debug("Latitude: " + currentLocation.getLatitude());
+                logger.debug("Longitude: " + currentLocation.getLongitude());
+            }
         }
 
-        logger.debug("Shops.shops.size() = " + Shops.shops.size());
+        Location currentLocation = locationProvider.getCurrentLocation();
+        if (currentLocation != null) {
+            double latitude = currentLocation.getLatitude();
+            double longitude = currentLocation.getLongitude();
+            logger.debug("Latitude: " + latitude + ", Longitude: " + longitude);
+        } else {
+            logger.debug("Location is null");
+        }
 
-        LocationProvider lp = new LocationProvider(this);
+//        //位置情報を取得
+//        locationProvider = new LocationProvider(this);
+//        requestLocationPermission();
 
         // RecyclerViewのインスタンスを取得
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
@@ -63,5 +94,37 @@ public class MainActivity extends AppCompatActivity {
         // ShopAdapterを作成し、RecyclerViewにセット
         ShopAdapter shopAdapter = new ShopAdapter(Shops.shops, this);
         recyclerView.setAdapter(shopAdapter);
+    }
+
+    private void requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        } else {
+            locationProvider.startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted.
+                locationProvider.startLocationUpdates();
+            } else {
+                // Permission denied.
+                // Handle the situation when user denies the permission.
+            }
+        }
+    }
+
+    private void debugLog() {
+        for (Shop shop : Shops.shops) {
+            logger.debug(shop.toString());
+        }
+
+        logger.debug("Shops.shops.size() = " + Shops.shops.size());
     }
 }
